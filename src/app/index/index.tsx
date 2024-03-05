e7e4ead2786de387030b5729c0cb377029bfbcf4
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-05 10:46:52 -0300

    feat: add styles and ingredients component to home screen

---
+++ src/app/index/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { Ingredients } from "@/components/Ingredients";
+
+import { styles } from "./styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Escolha {"\n"}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}>
+        Descubra receitas baseadas nos produtos que você escolheu.
+      </Text>
+
+      <Ingredients />
+    </View>
+  );
+}
