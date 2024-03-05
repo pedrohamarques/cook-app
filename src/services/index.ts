88ad214341168a7ba63d87d4b7816f80535ff41c
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-05 14:06:14 -0300

    feat: add supabase services

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import * as ingredients from "./ingredients-services";
+
+export const services = {
+  ingredients,
+  storage: {
+    imagePath:
+      "https://mqwedvgigyjtdlwlvtsx.supabase.co/storage/v1/object/public/ingredients",
+  },
+};
