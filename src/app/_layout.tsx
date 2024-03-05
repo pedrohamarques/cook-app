24c038f54e28ac9d638fefa85a2c0164eacd925e
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-05 10:09:04 -0300

    feat: add google font poppins

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Slot } from "expo-router";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+
+export default function Layout() {
+  const [isFontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+  return <Slot />;
+}
