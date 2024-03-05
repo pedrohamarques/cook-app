df0148f493725661a23d4492183609421601f447
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-05 15:15:43 -0300

    feat: add recipe and ingredients services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
 import * as ingredients from "./ingredients-services";
+import * as recipes from "./recipes-service";
+import * as preparations from "./preparations-service";
 
 export const services = {
   ingredients,
+  recipes,
+  preparations,
   storage: {
     imagePath:
       "https://mqwedvgigyjtdlwlvtsx.supabase.co/storage/v1/object/public/ingredients",
--- src/services/ingredients-services.ts
+++ src/services/ingredients-services.ts
@@ -9,3 +9,24 @@ export async function findAll() {
 
   return data ?? [];
 }
+
+export async function findByRecipeId(id: string) {
+  const { data } = await supabase
+    .from("recipes_ingredients")
+    .select("ingredients (id, name, image)")
+    .eq("recipe_id", id)
+    .returns<{ ingredients: IngredientResponse }[]>();
+
+  return data ? data.map((item) => item.ingredients) : [];
+}
+
+export async function findByIds(ids: string[]) {
+  const { data } = await supabase
+    .from("ingredients")
+    .select()
+    .in("id", ids)
+    .order("name")
+    .returns<IngredientResponse[]>();
+
+  return data ?? [];
+}
